79cee55ed5f7dbb422645fde2381f75a20112150
MyUsername <rosanio.c@gmail.com>
2025-02-28 11:20:30 +0100

    Snack 1 (Bonus)

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ getPostTitle(2)
     .then(data => console.log(data))
     .catch(error => console.error(error))
     .finally(console.log('Settled'))
+
+// Bonus
+
+function getPost(userId) {
+    const promise = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/users/${userId}`)
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(reject);
+    })
+    return promise;
+};
+
+getPost(91)
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+    .finally(console.log('Settled'))
