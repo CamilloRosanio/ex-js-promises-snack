408b2928b515619d168792a52cfbabd927988e46
MyUsername <rosanio.c@gmail.com>
2025-02-28 11:16:08 +0100

    Snack 1

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
 # SNACK 1
 ***********************************************************************/
 
+function getPostTitle(id) {
+    const promise = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(reject);
+    })
+    return promise;
+};
+
+getPostTitle(2)
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+    .finally(console.log('Settled'))
