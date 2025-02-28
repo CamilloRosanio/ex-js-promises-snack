e5367ddb49c9f031b58d3fd53c43764712f03bd5
MyUsername <rosanio.c@gmail.com>
2025-02-28 12:06:17 +0100

    Snack 2 (Bonus)

--- js/script.js
+++ js/script.js
@@ -13,10 +13,11 @@ function getPostTitle(id) {
 };
 
 // INVOCA
-getPostTitle(2)
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
-    .finally(console.log('Settled'))
+// getPostTitle(2)
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error))
+//     .finally(console.log('Settled'))
+
 
 // Bonus
 
@@ -31,10 +32,10 @@ function getPost(userId) {
 };
 
 // INVOCA
-getPost(91)
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
-    .finally(console.log('Settled'))
+// getPost(91)
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error))
+//     .finally(console.log('Settled'))
 
 
 
@@ -58,6 +59,51 @@ function lanciaDado(numero) {
 }
 
 // INVOCA
-lanciaDado(3)
+// lanciaDado(3)
+//     .then(messaggio => console.log(messaggio))
+//     .catch(error => console.error(error))
+
+
+// Bonus
+
+function creaLanciaDadoClosure() {
+    let ultimoLancio = 0;
+    return function lanciaDadoClosure(numero) {
+        console.log('Ultimo lancio: ' + ultimoLancio);
+        return new Promise((resolve, reject) => {
+            console.log('Sto lanciando il dado...');
+            setTimeout(() => {
+                const numeroDado = Math.floor(Math.random() * 6) + 1;
+                console.log('Risultato: ' + numeroDado);
+                if (numeroDado === ultimoLancio) {
+                    console.log('Incredibile! Stesso numero più volte di fila!');
+                }
+                if (numeroDado === numero) {
+                    resolve('Hai vinto!');
+                } else {
+                    reject('Hai perso. Non è uscito il numero su cui hai scommesso.');
+                }
+                ultimoLancio = numeroDado;
+            }, 1000);
+        });
+    };
+}
+
+const lanciaDadoUltimoLancio = creaLanciaDadoClosure();
+
+// INVOCA
+lanciaDadoUltimoLancio(3)
     .then(messaggio => console.log(messaggio))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
+
+setTimeout(() => lanciaDadoUltimoLancio(3)
+    .then(messaggio => console.log(messaggio))
+    .catch(error => console.error(error)), 2000);
+
+setTimeout(() => lanciaDadoUltimoLancio(3)
+    .then(messaggio => console.log(messaggio))
+    .catch(error => console.error(error)), 4000);
+
+setTimeout(() => lanciaDadoUltimoLancio(3)
+    .then(messaggio => console.log(messaggio))
+    .catch(error => console.error(error)), 6000);
